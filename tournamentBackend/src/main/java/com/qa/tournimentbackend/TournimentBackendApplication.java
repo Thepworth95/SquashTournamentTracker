e26a2287e0fa86619a716148802ffe99b9fbc1a3
Thepworth95 <thomashepworth@academytrainee.com>
2020-01-20 15:57:07 +0000

    springboot backend setup using h2 database

---
+++ tournamentBackend/src/main/java/com/qa/tournimentbackend/TournimentBackendApplication.java
@@ -0,0 +1,13 @@
+package com.qa.tournimentbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TournimentBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TournimentBackendApplication.class, args);
+    }
+
+}
---
+++ tournamentBackend/src/test/java/com/qa/tournimentbackend/TournimentBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.qa.tournimentbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TournimentBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
